03bab4125ccdd6ed06d09a508be098800526f08c
yoandreferreira <andredepaulacontato@gmail.com>
2022-12-06 22:37:57 -0300

    Primeiro commit

---
+++ src/test/java/io/github/yoandreferreira/shopvalidator/ShopValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yoandreferreira.shopvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
